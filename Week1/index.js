a4d40d28ddf9a69d7019e932e15973de468b5f52
Arno Törö <arno.toro@gmail.com>
2022-11-03 22:00:27 +0200

    changes

--- Week1/index.js
+++ Week1/index.js
@@ -7,52 +7,54 @@ if (document.readyState !== "loading") {
 }
 
 async function initializeCode() {
-  // dog api
-  const urlDogImg =
-    "https://dog.ceo/api/breed/terrier/westhighland/images/random";
-  const dogImg = await fetch(urlDogImg);
-  const imgJSON = await dogImg.json();
+    // dog api
+    const urlDogImg =
+        "https://dog.ceo/api/breed/terrier/westhighland/images/random";
+    const dogImg = await fetch(urlDogImg);
+    const imgJSON = await dogImg.json();
 
-  // wikipedia summary
-  const urlSummary =
-    "https://en.wikipedia.org/api/rest_v1/page/summary/West_Highland_White_Terrier";
-  const dogSummary = await fetch(urlSummary);
-  const summaryJSON = await dogSummary.json();
+    // wikipedia summary
+    const urlSummary =
+        "https://en.wikipedia.org/api/rest_v1/page/summary/West_Highland_White_Terrier";
+    const dogSummary = await fetch(urlSummary);
+    const summaryJSON = await dogSummary.json();
 
-  //create html instruments
-  const divContainer = document.getElementById('div-1')
-  const newDiv = divContainer.appendChild(document.createElement("div"));
-  const newHeader1 = newDiv.appendChild(document.createElement("h1"));
-  const newHeaderText = document.createTextNode("Westhighland Terrier");
-  const newContentDiv = newDiv.appendChild(document.createElement("div"));
-  const newP = newContentDiv.appendChild(document.createElement("p"));
-  const newPText = document.createTextNode(summaryJSON.extract);
-  const newImgDiv = newContentDiv.appendChild(document.createElement("div"));
-  const newImg = newImgDiv.appendChild(document.createElement("img"));
+    //create html instruments
+    const divContainer = document.getElementById('div-1')
+    const newDiv = divContainer.appendChild(document.createElement("div"));
+    const newHeader1 = newDiv.appendChild(document.createElement("h1"));
+    const newHeaderText = document.createTextNode("Westhighland Terrier");
+    const newContentDiv = newDiv.appendChild(document.createElement("div"));
+    const newP = newContentDiv.appendChild(document.createElement("p"));
+    const newPText = document.createTextNode(summaryJSON.extract);
+    const newImgDiv = newContentDiv.appendChild(document.createElement("div"));
+    const newImg = newImgDiv.appendChild(document.createElement("img"));
 
-  // container div
-  //divContainer.setAttribute('class', 'container')
+    // container div
+    divContainer.setAttribute('class', 'container')
 
-  // first div
-  newDiv.setAttribute("class", "wiki-item");
+    // first div
+    newDiv.setAttribute("class", "wiki-item");
+    newDiv.setAttribute("id", "div-1")
 
-  // header1
-  newHeader1.setAttribute("class", "wiki-header");
-  newHeader1.appendChild(newHeaderText);
+    // header1
+    newHeader1.setAttribute("class", "wiki-header");
+    newHeader1.appendChild(newHeaderText);
 
-  // content div
-  newContentDiv.setAttribute("class", "wiki-content");
+    // content div
+    newContentDiv.setAttribute("class", "wiki-content");
 
-  // paragraph
-  newP.setAttribute("class", "wiki-text");
-  newP.appendChild(newPText);
+    // paragraph
+    newP.setAttribute("class", "wiki-text");
+    newP.appendChild(newPText);
 
-  // imageDiv and image
-  newImgDiv.setAttribute("class", "img-container");
-  newImg.setAttribute("class", "wiki-img");
-  newImg.setAttribute("src", imgJSON.message);
+    // imageDiv and image
+    newImgDiv.setAttribute("class", "img-container");
+    newImg.setAttribute("class", "wiki-img");
+    newImg.setAttribute("src", imgJSON.message);
+
 }
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 4; i++) {
   initializeCode();
 }
