08f3014ce2729b722b431d9cbed675f5df1c81e2
arnotoro <arno.toro@gmail.com>
2022-11-28 16:44:48 +0200

    week4

---
+++ Week4/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fetch = require('node-fetch')
+const { response } = require('../app');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  async function getRecipe(url= '') {
+    const response = await fetch(url)
+    return response.json()
+  }
+
+  getRecipe('http://localhost:3000/recipe/pizza')
+    .then(json => {
+      res.render('index', {title: 'Recipe API',
+      name: json.name,
+      instructions: json.instructions,
+      ingredients: json.ingredients})
+  })
+})
+
+module.exports = router;
