5054131b516f97fe2dcc2f5f62149eb34a6a1df7
arnotoro <arno.toro@gmail.com>
2023-01-31 16:35:35 +0200

    completed week9

--- Week9/models/Users.js
+++ Week9/models/Users.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     email: String,
     password: String,
-    jwtID: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- Week9/routes/api.js
+++ Week9/routes/api.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-const User = require('../models/Users');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const {body, validationResult} = require('express-validator');
+
+const User = require('../models/Users');
+const Todos = require('../models/Todos');
 
 require('../auth/passport')(passport);
 
@@ -28,7 +31,7 @@ router.post('/user/login', (req, res, next) => {
           return next(err);
         } else if (result) {
           // create token
-          const token = jwt.sign({email: user.email}, process.env.SECRET, {expiresIn: 60 * 60});
+          const token = jwt.sign({email: user.email}, process.env.SECRET, {expiresIn: 120*5});
           res.status(200).json({success: true, token: token});
         } else {
           res.status(403).json({password: "Password is incorrect."});
@@ -37,56 +40,115 @@ router.post('/user/login', (req, res, next) => {
     }
   })
 });
-// register route
-router.post('/user/register', (req, res, next) => {
-  const {email, password} = req.body;
 
-  // check if users exist
-  User.findOne({email: email}, (err, user) => {
-    if (err) {
-      console.log(err);
-      return next(err);
-    } 
-    else if (user) {
-      res.status(403).json({email: "Email already exists."});
-    } else {
-      // hash password
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) {
-          console.log(err);
-          return next(err);
-        }
-        bcrypt.hash(password, salt, (err, hash) => {
+
+// register route
+router.post('/user/register', 
+  body('email').isEmail(),
+  body('password').isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1 
+  }), (req, res, next) => {
+    // check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    // check if users exist
+    User.findOne({email: email}, (err, user) => {
+
+      if (err) {
+        console.log(err);
+        return next(err);
+      } 
+
+      else if (user) {
+        res.status(403).json({email: "Email already exists."});
+      } 
+      else {
+        // hash password
+        bcrypt.genSalt(10, (err, salt) => {
           if (err) {
             console.log(err);
             return next(err);
-          } else {
-            // create new user
-            const newUser = new User({
-              email: email,
-              password: hash
-            });
-            newUser.save((err, user) => {
-              if (err) {
-                console.log(err);
-                return next(err);
-              } else {
-                res.status(200).json({success: true});
-              }
-            })
           }
+
+          bcrypt.hash(password, salt, (err, hash) => {
+            if (err) {
+              console.log(err);
+              return next(err);
+            } else {
+              // create new user
+              const newUser = new User({
+                email: email,
+                password: hash
+              });
+
+              newUser.save((err, user) => {
+                if (err) {
+                  console.log(err);
+                  return next(err);
+                } else {
+                  res.status(200).json({success: true});
+                }
+              })
+            }
+          })
         })
-      })
-    }
-  })
+      }
+    })
 });
  
 
 // secret route to test authentication
 router.get('/private', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-  res.status(200).send("You are authenticated!");
+  res.status(200).json({email: req.user.email});
 });
 
 
+// route for todos list
+router.post('/todos', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+
+  Todos.findOne({user: req.user._id}, (err, todos) => {
+    if (err) {
+      console.log(err)
+      return next(err);
+    } else if (todos) {
+      req.body.items.forEach(element => {
+        todos.items.push(element)
+      });
+      todos.save((err, todos) => {
+        if (err) {
+          console.log(err);
+          return next(err);
+        } else {
+          res.status(200).json({success: true, todos: todos});
+        }
+      })
+    } else {
+      const newTodos = new Todos({
+        user: req.user._id,
+        items: []
+      });
+      req.body.items.forEach(element => {
+        newTodos.items.push(element)
+      });
+        newTodos.save((err, todos) => {
+          if (err) {
+            console.log(err);
+            return next(err);
+          } else {
+            res.status(200).json({success: true, todos: todos});
+        }
+      })
+    }
+  })
+})
 
 module.exports = router;
